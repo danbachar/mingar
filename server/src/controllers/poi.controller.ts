b9d81e15f86fec70dcc638d44914da97a4f37e11
Dan Bachar <dandanbachar@gmail.com>
2019-11-23 17:07:10 +0100

    add route for poi controller

--- server/src/controllers/poi.controller.ts
+++ server/src/controllers/poi.controller.ts
@@ -2,7 +2,7 @@ import { PoiService } from '../services/poi.service';
 import { Controller, Get } from '@nestjs/common';
 import { POI } from 'entity/poi.entity';
 
-@Controller()
+@Controller('/api/poi')
 export class PoiController {
     constructor(private readonly poiService: PoiService) {}
 

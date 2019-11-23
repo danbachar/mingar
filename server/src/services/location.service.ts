a7131c3b10b830bbfc3d63a5fea81aebc0c359e5
dgloukhman <daniel.gloukhman@tum.de>
2019-11-23 17:19:44 +0100

    add getNearestPois

--- server/src/services/location.service.ts
+++ server/src/services/location.service.ts
@@ -2,9 +2,12 @@ import { LocationDTO } from './../DTO/location.dto';
 import { Injectable } from '@nestjs/common';
 import { sqrt,cos } from 'mathjs'
 import {POI} from "../entity/poi.entity";
+import {PoiService} from "./poi.service";
 
 @Injectable()
 export class LocationService {
+
+    constructor(private readonly poiService: PoiService) { }
 
     public calculateDistance(location1: LocationDTO, location2: LocationDTO): number {
         let lat =  (location1.lat - location2.lat) / 2 * 0.01745;
@@ -13,17 +16,16 @@ export class LocationService {
         return sqrt(dx * dx + dy * dy);
     }
 
-    public getNearestPois(myLocation: LocationDTO): POI[] {
+    public async getNearestPois(myLocation: LocationDTO): Promise<POI[]> {
         // change to getallPois
-        let listOfPois: POI[];
-        let sortedArray: POI[];
+        const listOfPois = await this.poiService.getAll();
 
-        sortedArray = listOfPois.sort((n1,n2) => {
+        const sortedArray = listOfPois.sort((n1, n2) => {
             const n1Location = new LocationDTO(n1.lat, n1.long);
             const n2Location = new LocationDTO(n2.lat, n2.long);
 
             return this.calculateDistance(myLocation, n1Location) - this.calculateDistance(myLocation, n2Location);
-        })
+        });
 
 
         return sortedArray;

4e026d4985a4135c5aa5890c491f6c032434ddcd
Dan Bachar <dandanbachar@gmail.com>
2019-11-23 22:20:57 +0100

    create ping method

--- server/src/controllers/app.controller.ts
+++ server/src/controllers/app.controller.ts
@@ -1,12 +1,10 @@
 import { Get, Controller } from '@nestjs/common';
-import { AppService } from '../services/app.service';
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
 
   @Get()
-  root(): string {
-    return this.appService.root();
+  ping(): string {
+    return 'pong';
   }
 }

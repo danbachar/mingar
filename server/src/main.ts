d6595e0da9b8c9eb01f6232aa22263d816ae8a7a
Dan Bachar <dandanbachar@gmail.com>
2019-11-23 16:23:29 +0100

    import app module from correct place

--- server/src/main.ts
+++ server/src/main.ts
@@ -1,5 +1,5 @@
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './module/app.module';
 import { LoggerFactory } from './utils/LoggerFactory';
 
 const logger = LoggerFactory(module);
